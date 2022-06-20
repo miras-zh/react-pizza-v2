4709c197f9fad081e45c8207f5a33ee811612173
mikos.zh8 <mikos.zh8@gmail.com>
2022-06-20 09:08:57 +0600

    feat: add components and write scripts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import CardPizza from "./components/CardPizza";
+import pizzasList from "./assets/pizzas.json";
+
+console.log("list>", pizzasList);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {pizzasList.map((obj) => (
+                <CardPizza {...obj}/>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Categories() {
+  const [activeCat, setActiveCat] = React.useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  const setActiveClass = (index) => {
+    setActiveCat(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map(
+          (value, index) => (<li
+          onClick={() => setActiveClass(index)}
+          className={activeCat === index ? "active" : ""}
+        >
+          {value}
+        </li>)
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
